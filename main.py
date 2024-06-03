4f7265f33dec4e22e475ca31c8aa86979fa9ded7
Mason <masoncfrancis@gmail.com>
2024-06-03 15:41:45 -0600

    Added stuff

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import argparse
 import time
+from rich.progress import track
 
 def countdown_timer(total_seconds):
-    while total_seconds:
+    while track(total_seconds):
         mins, secs = divmod(total_seconds, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end="\r")
@@ -13,16 +14,21 @@ def countdown_timer(total_seconds):
 
 def main():
     parser = argparse.ArgumentParser(description='Countdown Timer')
-    parser.add_argument('time', type=str, help='Time in MM:SS format')
+    parser.add_argument('time', type=str, help='Time in MM:SS or SS format')
     
     args = parser.parse_args()
     
     try:
-        mins, secs = map(int, args.time.split(':'))
-        total_seconds = mins * 60 + secs
+        # Try to split the input by ':'. If it fails, assume it's in seconds.
+        if ':' in args.time:
+            mins, secs = map(int, args.time.split(':'))
+            total_seconds = mins * 60 + secs
+        else:
+            total_seconds = int(args.time)
+        
         countdown_timer(total_seconds)
     except ValueError:
-        print("Invalid time format. Please use MM:SS format.")
+        print("Invalid time format. Please use MM:SS or SS format.")
 
 if __name__ == '__main__':
     main()
